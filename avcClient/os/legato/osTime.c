1405c5089fca39949d573d732a0388e304dbd59f
Andrew Au <AnAu@sierrawireless.com>
2019-09-06 14:54:35 -0700

    Add LWM2M object /3/0/13 Write support for updating device system clock
    Let an AV server use a Write into LWM2M object /3/0/13 to update a device's
    clock time so that HTTPS can work for subsequent OTAs.
    
    Resolves: LE-13214
    Related: LE-11503 STUDY-1545
    Change-Id: I7ade74b5b40e9887391595b07ae32d534de11743

--- avcClient/os/legato/osTime.c
+++ avcClient/os/legato/osTime.c
@@ -12,6 +12,19 @@
 #include <liblwm2m.h>
 #include "legato.h"
 #include "interfaces.h"
+#include "le_cfg_interface.h"
+#include <lwm2mcore/lwm2mcore.h>
+#include "dtlsConnection.h"
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Config tree root directory and node paths for clock time configurations
+ * Note: These 2 defines are temporary. After the new Legato Clock Service interface le_clkSync is
+ * added, these defines will be there in le_clkSync.api and these ones can be removed.
+ */
+//--------------------------------------------------------------------------------------------------
+#define LE_CLKSYNC_CONFIG_TREE_ROOT_SOURCE          "clockTime:/source"
+#define LE_CLKSYNC_CONFIG_NODE_SOURCE_AVC_TIMESTAMP "timeStamp"
 
 //--------------------------------------------------------------------------------------------------
 /**
@@ -31,4 +44,75 @@ time_t lwm2m_gettime
     LE_DEBUG("Device time: %ld", deviceTime.sec);
 
     return deviceTime.sec;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Function to perform an immediate system clock update using the clock time value set on the config
+ * tree.  Leave this value there not reset after use so that it can serve as a last resort clock
+ * time more up-to-date than 1970/1/1 in case the device after a restart cannot succeed in any way
+ * e.g. via QMI, TP, NTP, etc., to get the current clock time.
+ * The use of this last-resort clock time hasn't been implemented yet, but will be soon under the
+ * new Clock Service in Legato.
+ */
+//--------------------------------------------------------------------------------------------------
+void lwm2mcore_UpdateSystemClock
+(
+    void* connP
+)
+{
+    dtls_Connection_t* connPtr = (dtls_Connection_t*)connP;
+    le_cfg_IteratorRef_t cfg;
+    le_result_t result;
+    le_clk_Time_t t;
+    time_t clockStamp;
+
+    cfg = le_cfg_CreateReadTxn(LE_CLKSYNC_CONFIG_TREE_ROOT_SOURCE);
+    if (!cfg)
+    {
+        LE_WARN("No clock stamp given to update the system clock");
+        return;
+    }
+    if (!le_cfg_NodeExists(cfg, LE_CLKSYNC_CONFIG_NODE_SOURCE_AVC_TIMESTAMP))
+    {
+        LE_WARN("No clock stamp given to update the system clock");
+        le_cfg_CancelTxn(cfg);
+        return;
+    }
+
+    clockStamp = le_cfg_GetInt(cfg, LE_CLKSYNC_CONFIG_NODE_SOURCE_AVC_TIMESTAMP, 0);
+    le_cfg_CancelTxn(cfg);
+
+    if (clockStamp <= 0)
+    {
+        LE_WARN("No valid clock stamp retrieved to update the system clock");
+        return;
+    }
+
+    t = le_clk_GetAbsoluteTime();
+    LE_INFO("device's time %ld sec %ld usec before", (long)t.sec, (long)t.usec);
+
+    t.sec = clockStamp;
+    t.usec = 0;
+
+    result = le_clk_SetAbsoluteTime(t);
+    LE_INFO("Result in setting system clock time: %d", result);
+
+    t = le_clk_GetAbsoluteTime();
+    LE_INFO("device's time %ld  %ld usec after", (long)t.sec, (long)t.usec);
+
+    LE_INFO("Triggering DTLS rehandshake after system clock update");
+
+    if (!connPtr)
+    {
+        LE_DEBUG("No need to initiate a DTLS handshake");
+        return;
+    }
+
+    // Initiate a DTLS handshake after the system clock has changed so that DTLS can continue to
+    // work for AVC
+    if (0 != dtls_Rehandshake(connPtr, false))
+    {
+        LE_ERROR("Unable to perform a DTLS rehandshake for connection %p", connPtr);
+    }
 }
--- avcClient/osPortDevice.c
+++ avcClient/osPortDevice.c
@@ -20,6 +20,7 @@
 #endif
 #include "avcServer.h"
 #include "avcSim.h"
+#include "sessionManager.h"
 
 #ifdef LE_CONFIG_CUSTOM_OS
 #include "version.h"
@@ -27,6 +28,7 @@
 #endif
 
 #include "avcClient.h"
+#include <lwm2mcore/lwm2mcore.h>
 
 //--------------------------------------------------------------------------------------------------
 /**
@@ -153,6 +155,16 @@
  */
 //--------------------------------------------------------------------------------------------------
 #define DEFAULT_REBOOT_TIMER  2
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Config tree root directory and node paths for clock time configurations
+ * Note: These 2 defines are temporary. After the new Legato Clock Service interface le_clkSync is
+ * added, these defines will be there in le_clkSync.api and these ones can be removed.
+ */
+//--------------------------------------------------------------------------------------------------
+#define LE_CLKSYNC_CONFIG_TREE_ROOT_SOURCE          "clockTime:/source"
+#define LE_CLKSYNC_CONFIG_NODE_SOURCE_AVC_TIMESTAMP "timeStamp"
 
 //--------------------------------------------------------------------------------------------------
 /**
@@ -987,6 +999,47 @@ lwm2mcore_Sid_t lwm2mcore_GetDeviceCurrentTime
     {
         return LWM2MCORE_ERR_GENERAL_ERROR;
     }
+
+    return LWM2MCORE_ERR_COMPLETED_OK;
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Initiate the setting of the device time (UNIX time in seconds) to the given clock time by
+ * archiving this input time and registering a post LWM2M request processing handler so that
+ * after a response is sent out over for this device clock setting request, then the actual clock
+ * change execution thru this handler can be carried out. This is because if this execution isn't
+ * carried out in this chronological arrangement, the sudden clock change will fail the sending
+ * out of the response, e.g. over DTLS, and the subsequent outstanding LWM2M jobs on its queue.
+ * In another word, the response has to go out 1st before the clock change happens.
+ *
+ * @return
+ *      - LWM2MCORE_ERR_COMPLETED_OK if the treatment succeeds
+ *      - LWM2MCORE_ERR_GENERAL_ERROR if the treatment fails
+ */
+//--------------------------------------------------------------------------------------------------
+lwm2mcore_Sid_t lwm2mcore_SetDeviceCurrentTime
+(
+    uint64_t inputTime  ///< [IN] Current clock time given
+)
+{
+    LE_DEBUG("input time %ld", (long)inputTime);
+
+    if (!lwm2mcore_AddPostRequestHandler(lwm2mcore_UpdateSystemClock))
+    {
+        LE_ERROR("Failed to initiate clock time update");
+        return LWM2MCORE_ERR_GENERAL_ERROR;
+    }
+
+    // Save inputTime onto config tree to make this given value persistent across system restart
+    // in case it is needed as a last resort system clock time. This can happen to a device
+    // after a restart & its total failure to get any more up-to-date clock so that its system
+    // clock has to restart from 1970/1/1 again. Then this last archived clock time provided by
+    // an AV server is still relatively more up-to-date for use.
+    le_cfg_IteratorRef_t cfg;
+    cfg = le_cfg_CreateWriteTxn(LE_CLKSYNC_CONFIG_TREE_ROOT_SOURCE);
+    le_cfg_SetInt(cfg, LE_CLKSYNC_CONFIG_NODE_SOURCE_AVC_TIMESTAMP, inputTime);
+    le_cfg_CommitTxn(cfg);
 
     return LWM2MCORE_ERR_COMPLETED_OK;
 }
